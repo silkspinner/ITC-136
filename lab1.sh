ecfe8be789e9328c93920d13eff62ddb3c7edd31
Ron Nims <rleenims@gmail.com>
2017-04-04 14:48:35 -0700

    First Linux command line primer

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,10 @@
-# This is a Linux script comment
+# itc136 April 4, Ron Nims
+# First Linux command line primer
+pwd     # Command to Print Working Directory, (Where)
+whoami  # Command to return user ID, (Who)
+ls      # Command to show contents of directory, (What)
+        # -l switch displays information about contents of directory
+cd      # Command to change directory, (Navigation)
+date    # Command to show machine time, (When)
+
+
